5a0edb0309b1bafd3f9d5e3740bc4815a1a8b51a
Noweli <patryklewonn@gmail.com>
2020-10-25 21:26:20 +0100

    Light sensor propagates data correctly through HTTP request

--- ESPWeatherControl/ServerController.ino
+++ ESPWeatherControl/ServerController.ino
@@ -3,6 +3,7 @@ void SetUpServer(){
   server.on("/temperature", Handle_TemperatureRequest);
   server.on("/humidity", Handle_HumidityRequest);
   server.on("/sound", Handle_SoundSensorRequest);
+  server.on("/light", Handle_LightSensorRequest);
   server.onNotFound(Handle_NotFound);
   
   server.begin();
