57cc1168adc33cf0344d33c8b166798da944da91
Noweli <patryklewonn@gmail.com>
2020-12-03 23:41:43 +0100

    Added temperature request

--- SmartHomeAPI/SmartHomeAPI/Controllers/RequestController.cs
+++ SmartHomeAPI/SmartHomeAPI/Controllers/RequestController.cs
@@ -1,7 +1,28 @@
-﻿namespace SmartHomeAPI.Controllers
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using SmartHomeAPI.DTOs;
+
+namespace SmartHomeAPI.Controllers
 {
     public class RequestController : BaseApiController
     {
+        private static readonly HttpClient client = new HttpClient();
         
+        [Authorize]
+        [HttpGet("temperature")]
+        public async Task<ActionResult<WeatherDTO>> GetTemperature()
+        {
+            decimal temperature =
+                decimal.Parse(JObject.Parse(client.GetStringAsync("http://192.168.0.144/temperature").Result)["temperature"].ToString());
+            decimal humidity = decimal.Parse(JObject.Parse(client.GetStringAsync("http://192.168.0.144/humidity").Result)["humidity"].ToString());
+            return new WeatherDTO
+            {
+                Temperature = temperature,
+                Humidity = humidity
+            };
+        }
     }
 }
