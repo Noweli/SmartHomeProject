dd561ff73496ebc3b318e9da4501c381b0a4244d
Noweli <patryklewonn@gmail.com>
2020-12-03 23:13:30 +0100

    Added register and login

--- SmartHomeAPI/SmartHomeAPI/DTOs/LoginDTO.cs
+++ SmartHomeAPI/SmartHomeAPI/DTOs/LoginDTO.cs
@@ -2,7 +2,7 @@
 
 namespace SmartHomeAPI.DTOs
 {
-    public class RegisterDTO
+    public class LoginDTO
     {
         [Required] public string UserName { get; set; }
         [Required] public string Password { get; set; }
